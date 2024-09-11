82dc4f04fb602be855e646fe4a7a9a4ead5304f4
Jasper Dijt <jasper.dijt@lunatech.nl>
2024-09-11 16:26:21 +0200

    Simplify token retrieval.

--- app/services/PeopleService.scala
+++ app/services/PeopleService.scala
@@ -28,13 +28,10 @@ class PeopleService @Inject()(configuration: Configuration,
   private val clientSecret: String = configuration.get[String]("lunagraph.client.secret")
   private val clientIssuer: String = configuration.get[String]("lunagraph.client.issuer")
 
-  private var tokenExpiry: Option[Instant] = None
-  private var token: Option[String] = None
-
   def getPersons: Future[Either[String, Seq[Person]]] = for {
-    _ <- ensureToken()
+    token <- getToken()
     wsRes <- ws.url(s"${baseUrl}/graphql")
-      .withHttpHeaders("Authorization"-> s"Bearer ${token.get}")
+      .withHttpHeaders("Authorization"-> s"Bearer ${token}")
       .post(Json.toJsObject(Map("query" -> query)))
     res = wsRes.status match {
         case Status.OK =>
@@ -46,27 +43,20 @@ class PeopleService @Inject()(configuration: Configuration,
   } yield res
 
 
-  private def ensureToken(): Future[Either[String, Unit]] = {
-    val cutOff = Instant.now().minusSeconds(30)
-    if (token == None || tokenExpiry == None || tokenExpiry.get.isAfter(cutOff)){
-        for {
-          clientResponse <- ws.url(s"${clientIssuer}/protocol/openid-connect/token")
-            .withAuth(clientId, clientSecret, WSAuthScheme.BASIC)
-            .post(Map("grant_type" -> Seq("client_credentials")))
-          parsedResponse = clientResponse.status match {
-            case Status.OK =>
-              val jsonResponse = Json.parse(clientResponse.body)
-              val expiresIn = (jsonResponse \ "expires_in").get.as[Int]
-              token = Some((jsonResponse \ "access_token").get.as[String])
-              tokenExpiry = Some(Instant.now().plusSeconds(expiresIn))
-              Right(())
-            case _ =>
-              logger.error(clientResponse.body)
-              Left(clientResponse.body)
-          }
-        } yield parsedResponse
-    } else {
-      Future.successful(Right(()))
-    }
+  private def getToken(): Future[Either[String, String]] = {
+    for {
+      clientResponse <- ws.url(s"${clientIssuer}/protocol/openid-connect/token")
+        .withAuth(clientId, clientSecret, WSAuthScheme.BASIC)
+        .post(Map("grant_type" -> Seq("client_credentials")))
+      parsedResponse = clientResponse.status match {
+        case Status.OK =>
+          val jsonResponse = Json.parse(clientResponse.body)
+          val token = (jsonResponse \ "access_token").get.as[String]
+          Right(token)
+        case _ =>
+          logger.error(clientResponse.body)
+          Left(clientResponse.body)
+      }
+    } yield parsedResponse
   }
 }
