84b6bb823867d2b017ed8699441cd8fa21fed6c0
Jasper Dijt <jasper.dijt@lunatech.nl>
2024-09-12 10:28:56 +0200

    Disable clean-on-startup.

--- app/actors/Cleaner.scala
+++ app/actors/Cleaner.scala
@@ -12,7 +12,6 @@ import scala.concurrent.ExecutionContext
 @Singleton
 class CleanerScheduler @Inject()(val system: ActorSystem, @Named("cleaner-actor") val cleanerActor: ActorRef)(implicit ec: ExecutionContext) {
   QuartzSchedulerExtension(system).schedule("Clean", cleanerActor, Clean)
-  cleanerActor ! Clean
 }
 
 class Cleaner @Inject()(unifiService: UnifiService,
